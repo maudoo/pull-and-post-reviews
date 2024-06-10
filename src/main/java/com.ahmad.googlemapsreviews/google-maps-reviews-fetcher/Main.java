41164650b0aedf32cf1ce62b4e40e430b19655bd
maudoo <mhasan13@hawk.iit.edu>
2024-06-10 14:25:02 -0500

    tweaks

--- src/main/java/com.ahmad.googlemapsreviews/google-maps-reviews-fetcher/Main.java
+++ src/main/java/com.ahmad.googlemapsreviews/google-maps-reviews-fetcher/Main.java
@@ -1,7 +1,19 @@
-package com.ahmad.googlemapsreviews;
+
+import com.google.maps.GeocodingApi;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        GeoApiContext context = new GeoApiContext.Builder();
+        apiKey("Your API_KEY"); // Replace with your actual API key
+        build();
+
+        try {
+            GeocodingResult[] results = GeocodingApi.geocode(context, "1600 Amphitheatre Parkway, Mountain View, CA").await();
+            System.out.println(results[0].formattedAddress);
+        } catch (Exception e) {
+            System.err.println(e.getMessage());
+        }
     }
 }
